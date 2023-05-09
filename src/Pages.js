b00f76feab9d4aad66fa5056bfdef854e1a5efce
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-05-09 09:48:34 -0300

    feature-19-navbar

--- src/Pages.js
+++ src/Pages.js
@@ -2,11 +2,15 @@ import React from "react";
 //yarn add react-router-dom
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserList from "./pages/users/UserList";
+import Navbar from "./config/navbar/Navbar";
+import Home from "./pages/Home";
 
 export default function Pages() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
+                <Route exact path="/" element={<Home />} />
                 <Route exact path="/user-list" element={<UserList />} />
             </Routes>
         </BrowserRouter>
