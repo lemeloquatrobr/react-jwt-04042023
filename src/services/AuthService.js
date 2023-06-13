04cc26cbc8d972d6980a3df7bab0e96de4b8ed72
Lemelo Quatro Br <lemeloquatrobr@gmail.com>
2023-06-13 16:07:44 -0300

    feature-30-signin-page

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -10,3 +10,8 @@ export const getCheckToken = async (token) => {
     const response = await apiAxios.get("/auth/check-token", headers);
     return response.data;
 }
+
+export const postSignin = async ({email, password}) => {
+    const response = await apiAxios.post("/auth/signin", {email, password});
+    return response.data;
+}
